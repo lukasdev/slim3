5c5b7bfdd44e5f439b9e752f4ab1080ed3402eeb
Lucas Silva <dev.lucassilva@gmail.com>
2016-05-28 17:28:15 -0300

    renderizando views com twig

--- app/DownsMaster/Controllers/HomeController.php
+++ app/DownsMaster/Controllers/HomeController.php
@@ -1,8 +1,25 @@
 <?php
     namespace DownsMaster\Controllers;
+    use DownsMaster\Controllers\Controller;
 
-    class HomeController{
+    class HomeController extends Controller{
         public function index($request, $response, $args){
-            echo 'Estou na home via controller';
+            $clientes = [
+                0 => [
+                    'nome' => 'Fulano de Tal',
+                    'salario' => 1500
+                ],
+                1 => [
+                    'nome' => 'Ciclano da Silva',
+                    'salario' => 1800
+                ],
+                2 => [
+                    'nome' => 'Beltrano dos Santos',
+                    'salario' => 2800
+                ]                
+            ];
+            return $this->view->render($response, 'home.twig', [
+                'clientes' => $clientes
+            ]);
         }
     }
--- index.php
+++ index.php
@@ -7,9 +7,24 @@
     ]);
 
     $container = $app->getContainer();
+    $container['view'] = function($container){
+        $folder = __DIR__;
+        $view = new \Slim\Views\Twig($folder.'/app/views',[
+            'cache' => false
+        ]);
 
-    $container['HomeController'] = function($container) use ($app){
-        return new DownsMaster\Controllers\HomeController();
+        $view->addExtension(new \Slim\Views\TwigExtension(
+            $container->router,
+            $container->request->getUri()
+        ));
+
+        return $view;
     };
 
+    $container['HomeController'] = function($container) use ($app){
+        return new DownsMaster\Controllers\HomeController($container);
+    };
+
+
+    $app->get('/', 'HomeController:index');
     $app->run();
