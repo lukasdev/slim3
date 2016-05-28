580457cf5a8825b8c6797dec84c1ee5bdd452ca7
Lucas Silva <dev.lucassilva@gmail.com>
2016-05-28 17:10:50 -0300

    Apaguei as rotas

--- index.php
+++ index.php
@@ -12,29 +12,4 @@
         return new DownsMaster\Controllers\HomeController();
     };
 
-    $app->get('/post/{categoria}/{slug}', function($request, $response, $args){
-        var_dump($request->getAttribute('categoria'));
-        var_dump($request->getAttribute('slug'));
-        echo '<br />'.$this->teste;
-    });
-
-    $app->post('/contato', function($request, $response, $args){
-       /*
-        Um codigo complexo aqui foi executado
-       */
-
-        return $response->withStatus(200)->write('Success');
-        #return $response->withRedirect($this->router->pathFor('home'));
-    });
-
-    $app->get('/', 'HomeController:index')->setName('home');
-
-    $app->get('/contato', function($request, $response, $args){
-        $pathContato = $this->router->pathFor('contato');
-
-        echo '<form action="'.$pathContato.'" method="post">';
-        echo '<input type="submit" name="teste" value="Enviar" />';
-        echo '</form>';
-    })->setName('contato');
-
     $app->run();
